b3b0c41ebc5fe5b75d8403362b8e5443ff2a38ea
nishant <nishun2005@gmail.com>
2024-10-21 14:56:22 -0700

    more stuff

--- QC/py-qcrunner/app/main.py
+++ QC/py-qcrunner/app/main.py
@@ -178,6 +178,8 @@ def voilin_plot():
     print("-------TESTING-------")
     import imageio
     import plotly.express as px
+    import plotly as pltly
+    import kaleido
 
     img = imageio.imread(png_file)
     binary_img = (img > 128).astype(int)
@@ -186,7 +188,9 @@ def voilin_plot():
 
     fig = px.imshow(binary_img, binary_string=True)
     #for now write to s3 to test
-    fig.write_image("plotly_test.png")
+    fig.write_image("plotly_test.png", engine="kaleido")
+    fig_dict = json.loads(fig.to_json())
+    print(fig_dict)
     upload_plot_to_s3(f"{s3_plots_dir}/plotly_test.png", "plotly_test.png")
 
     # Create S3 object key for quality control data
