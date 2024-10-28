095f52ce99b44aaa938a917bd43de23478cde91b
nishant <nishun2005@gmail.com>
2024-10-28 10:48:54 -0700

    n_genes ->n_genes_by_counts

--- QC/py-qcrunner/app/main.py
+++ QC/py-qcrunner/app/main.py
@@ -168,12 +168,12 @@ def voilin_plot():
 
     print("Creating violin df")
     print(adata.obs.columns)
-    data_df = adata.obs["n_genes", "total_counts", "pct_counts_mt"]
+    data_df = adata.obs["n_genes_by_counts", "total_counts", "pct_counts_mt"]
 
     print("Creating violin json")
     data_for_highcharts = {
         index:{
-                "n_genes":row['n_genes'],
+                "n_genes":row['n_genes_by_counts'],
                 "total_counts": row["total_counts"],
                 "pct_counts_mt":row["pct_counts_mt"]
         }
